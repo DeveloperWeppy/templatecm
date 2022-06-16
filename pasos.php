3bf7600d50a7802fe3627427cb76e656047896e3
WeppyDev <104014309+DeveloperWeppy@users.noreply.github.com>
2022-06-16 17:00:52 -0500

    16/06/500

--- pasos.php
+++ pasos.php
@@ -73,9 +73,33 @@
                 <label for="position">Barrio</label>
                 <input type="text" name="position" id="position" />
             </div>
-            <div class="input-group col-6">
-                <label for="position" >Foto de documento</label>
-                <input type="file" accept="image/png, image/gif, image/jpeg, image/jpg" name="position" id="position" />
+            <div class=" col-12">
+                                <!-- Upload image input-->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="input-group mb-3 px-2 py-2 rounded-pill bg-white shadow-sm">
+                                            <input id="upload" type="file" accept="image/png, image/gif, image/jpeg, image/jpg" onchange="readURL(this);" name="imgLogo" class="form-control dnone border-0">
+                                            <label id="upload-label" for="upload" class="pd2 font-weight-light w50 text-muted">Foto frontal del documento</label>
+                                            <div class="input-group-append w50">
+                                                <label for="upload" class="btn w100 btn-light m-0 rounded-pill px-4"> <i class="fas fa-upload mr-2  white"></i><small class="text-uppercase font-weight-bold white">Subir Logo</small></label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                    <div class="image-area mt-4"><img id="imageResult" src="assets/img/background/preview.png" alt="" class="img-fluid rounded shadow-sm mx-auto d-block"></div>
+                                    </div>
+                                </div>
+
+                               <!-- <div class="input-group mb-3 px-2 py-2 rounded-pill bg-white shadow-sm">
+                                    <input id="upload" type="file" accept="image/png, image/gif, image/jpeg, image/jpg" onchange="readURL(this);" name="imgLogo" class="form-control border-0">
+                                    <label id="upload-label" for="upload" class="font-weight-light text-muted">Imágen</label>
+                                    <div class="input-group-append">
+                                        <label for="upload" class="btn btn-light m-0 rounded-pill px-4"> <i class="fas fa-upload mr-2 text-muted"></i><small class="text-uppercase font-weight-bold text-muted">Subir Logo</small></label>
+                                    </div>
+                                </div> -->
+
+                                <!-- Uploaded image area-->
+                <div class="image-area mt-4"><img id="imageResult" src="#" alt="" class="img-fluid rounded shadow-sm mx-auto d-block"></div>
             </div>
             <div class="boton">
                 <label for="position"></label>
@@ -83,14 +107,79 @@
             </div>
         </div>
         <div class="form-step">
-            <div class="input-group">
-                <label for="phone">Phone</label>
-                <input type="text" name="phone" id="phone" />
+            <!-- |==========================================| -->
+        <!-- |=====|| Service Start ||===============| -->
+        <section class="pricing1">
+            <div class="content_box_100">
+                <div class="container">
+                    <div class="row no-gutters pricing1__row">
+                        <div class=" color_white">
+                            <div class="pricing1__item">
+                                <div class="pricing1__wrapper text-center">
+                                    <div class="pricing1__thumb--style">
+                                        <div class="pricing1__thumb">
+                                            <img src="assets/img/png-icon/png-icon-19.png" alt="Image">
+                                        </div>
+                                    </div>
+                                    <div class="pricing1__content mt-85">
+                                        <h4>Basic</h4>
+                                        <p class="m-0">For a month</p>
+                                        <h3>$39</h3>
+                                        <ul>
+                                            <li>
+                                                <span class="m-0">Weekly health check-ups</span>
+                                            </li>
+                                            <li>
+                                                <span class="m-0">Lab test system an hour</span>
+                                            </li>
+                                            <li>
+                                                <span class="m-0">Free diet consultation</span>
+                                            </li>
+                                            <li>
+                                                <span class="m-0">Custom exercise plans</span>
+                                            </li>
+                                        </ul>
+                                        <a href="#" class="btn8">Get Started</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class=" color_white">
+                            <div class="pricing1__item">
+                                <div class="pricing1__wrapper text-center">
+                                    <div class="pricing1__thumb--style">
+                                        <div class="pricing1__thumb">
+                                            <img src="assets/img/png-icon/png-icon-19.png" alt="Image">
+                                        </div>
+                                    </div>
+                                    <div class="pricing1__content mt-85">
+                                        <h4>Professional</h4>
+                                        <p class="m-0">For a month</p>
+                                        <h3>$59</h3>
+                                        <ul>
+                                            <li>
+                                                <span class="m-0">Weekly health check-ups</span>
+                                            </li>
+                                            <li>
+                                                <span class="m-0">Lab test system an hour</span>
+                                            </li>
+                                            <li>
+                                                <span class="m-0">Free diet consultation</span>
+                                            </li>
+                                            <li>
+                                                <span class="m-0">Custom exercise plans</span>
+                                            </li>
+                                        </ul>
+                                        <a href="#" class="btn8">Get Started</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="input-group">
-                <label for="email">Email</label>
-                <input type="text" name="email" id="email" />
-            </div>
+        </section>
+        <!-- |==========================================| -->
             <div class="btns-group">
                 <a href="#" class="btn btn-prev">Siguiente</a>
                 <a href="#" class="btn btn-next">Atras</a>
@@ -106,6 +195,7 @@
 </a>
 <!-- |=====|| ScrollToTop End ||=================| -->
 <!-- |==========================================| -->
+
 <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
 <script src="assets/js/vendor/jquery-3.3.1.min.js"></script>
 <script src="assets/js/vendor/popper.min.js"></script>
@@ -128,5 +218,41 @@
 <script src="assets/js/main.js"></script>
 <!-- |=====|| All js End ||=================| -->
 <!-- |==========================================| -->
+
+<script>
+    /*  ==========================================
+          SHOW UPLOADED IMAGE
+      * ========================================== */
+      function readURL(input) {
+          if (input.files && input.files[0]) {
+              var reader = new FileReader();
+
+              reader.onload = function (e) {
+                  $('#imageResult')
+                      .attr('src', e.target.result);
+              };
+              reader.readAsDataURL(input.files[0]);
+          }
+      }
+
+      $(function () {
+          $('#upload').on('change', function () {
+              readURL(input);
+          });
+      });
+
+      /*  ==========================================
+          SHOW UPLOADED IMAGE NAME
+      * ========================================== */
+      var input = document.getElementById( 'upload' );
+      var infoArea = document.getElementById( 'upload-label' );
+
+      input.addEventListener( 'change', showFileName );
+      function showFileName( event ) {
+        var input = event.srcElement;
+        var fileName = input.files[0].name;
+        infoArea.textContent = 'Archivo: ' + fileName;
+      }
+</script>
 
 </html>
